10fec45f6f721bafd84a4d9c1b6bcc6574928d8f
adisbladis <adisbladis@gmail.com>
2020-08-11 16:39:23 +0200

    Remove messages comment

--- messages.go
+++ messages.go
@@ -1,12 +1,10 @@
 package main
 
-// Request is a convenience type that we'll extend in the next iteration
 type Request struct {
 	input  Input
 	output Output
 }
 
-// Response is a convenience type that we'll extend in the next iteration
 type Response struct {
 	status string
 }
