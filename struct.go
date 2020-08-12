50855149e1640948ed8d77d3665390f5cca52cd5
adisbladis <adisbladis@gmail.com>
2020-08-12 10:50:43 +0200

    Add comments to input struct

--- struct.go
+++ struct.go
@@ -1,16 +1,21 @@
 package main
 
-// Corresponds to Trillian Log LeafValue
+// Input - Corresponds to Trillian Log LeafValue
 type Input struct {
 	inputHash string
 }
 
+// Create a new Input instance from inputHash string
 func newInput(inputHash string) *Input {
 	return &Input{
 		inputHash: inputHash,
 	}
 }
 
+// Marshal - A convenience method
+// While we probably don't want any extra fields in the input
+// this method exists in case we want to extend the data type
+// at some point later on
 func (t *Input) Marshal() ([]byte, error) {
 	return []byte(t.inputHash), nil
 }
