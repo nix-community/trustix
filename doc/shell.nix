dd08b9b6fcbcb34478a7ccd85706cdd0f42aa330
adisbladis <adisbladis@gmail.com>
2020-08-17 10:03:29 +0200

    Add env to build docs

---
+++ doc/shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  tl = (pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-medium wrapfig ulem capt-of
+    titlesec preprint enumitem paralist ctex environ svg
+    beamer trimspaces zhnumber changepage framed pdfpages
+    fvextra minted upquote ifplatform xstring; });
+
+  pythonEnv = pkgs.python3.withPackages(ps: [
+    ps.pygments
+  ]);
+
+in pkgs.mkShell {
+  buildInputs = [
+    pythonEnv
+    pkgs.pandoc
+    pkgs.haskellPackages.pandoc-citeproc
+    pkgs.haskellPackages.pandoc-crossref
+    pkgs.plantuml
+    tl
+  ];
+}
