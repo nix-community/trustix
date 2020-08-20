9d0b88fd89d5e77ee30e278dd70b2b6115583664
adisbladis <adisbladis@gmail.com>
2020-08-20 11:19:49 +0200

    Make git tree update recursively

--- pytree/trustix/__main__.py
+++ pytree/trustix/__main__.py
@@ -7,4 +7,6 @@ if __name__ == '__main__':
     email = "trustix@example.com"
 
     r = repo.Repository(repo_path, name=name, email=email)
-    print(r)
+
+    r.add_leaf("w9yy7v61ipb5rx6i35zq1mvc2iqfmps1", b"sha256:1mi14cqk363wv368ffiiy01knardmnlyphi6h9xv6dkjz44hk30i")
+    r.add_leaf("w9yy7v61ipb5rx6i35zq1mvc2iqfmpbb", b"sha256:1mi14cqk363wv368ffiiy01knardmnlyphi6h9xv6dkjz44hk30a")
--- pytree/trustix/repo.py
+++ pytree/trustix/repo.py
@@ -1,7 +1,13 @@
-import pygit2 as git
+import pygit2 as git  # type: ignore
+import itertools
+import string
 import os.path
 import typing
 import time
+
+
+# Precompute possible leaf permutations
+_path_permutations: typing.List[str] = list("".join(p) for p in itertools.permutations(string.hexdigits.lower(), 2))
 
 
 def repo_create(path: str) -> git.Repository:
@@ -27,6 +33,41 @@ def repo_open(path: str) -> git.Repository:
     return r
 
 
+def shard(input: str) -> typing.Tuple[str, ...]:
+    """
+    Decide where an input to the tree ends up
+    """
+    depth = int(len(input) / 2)
+    return tuple(
+        input[2 * i : 2 * i + 2]
+        for i in range(depth)
+    )
+
+
+def auto_insert(repo, treebuilder, path, thing, mode):
+    print(path)
+    path_parts = path.split('/', 1)
+    if len(path_parts) == 1:
+        treebuilder.insert(path, thing, mode)
+        return treebuilder.write()
+
+    subtree_name, sub_path = path_parts
+    tree_oid = treebuilder.write()
+    tree = repo.get(tree_oid)
+    try:
+        entry = tree[subtree_name]
+        assert entry.filemode == git.GIT_FILEMODE_TREE,\
+            '{} already exists as a blob, not a tree'.format(entry.name)
+        existing_subtree = repo.get(entry.hex)
+        sub_treebuilder = repo.TreeBuilder(existing_subtree)
+    except KeyError:
+        sub_treebuilder = repo.TreeBuilder()
+
+    subtree_oid = auto_insert(repo, sub_treebuilder, sub_path, thing, mode)
+    treebuilder.insert(subtree_name, subtree_oid, git.GIT_FILEMODE_TREE)
+    return treebuilder.write()
+
+
 class Repository:
 
     _repo: git.Repository
@@ -50,6 +91,32 @@ class Repository:
             self._repo = repo_create(repo_path)
             self._tree = self._repo.TreeBuilder().write()
             self.write_commit(message="Init log")
+            # TODO: Create an empty root hash
+
+        self._builder = self._repo.TreeBuilder(self._tree)
+
+    def _write_tree(self, full_path: typing.Tuple[str, ...], content: bytes):
+        """Write path to tree and update the internal tree reference"""
+
+        oid = self._repo.write(git.GIT_OBJ_BLOB, content)
+
+        visited = []
+        for p in full_path[:-1]:
+            visited.append(p)
+            builder = self._repo.TreeBuilder(self._tree)
+            builder.insert("/".join(visited), self._tree, git.GIT_FILEMODE_TREE)
+            self._tree = builder.write()
+
+        builder = self._repo.TreeBuilder(self._tree)
+        builder.insert("/".join(full_path), oid, git.GIT_FILEMODE_BLOB)
+        self._tree = builder.write()
+
+    def add_leaf(self, input: str, content: bytes):
+        sharded = os.path.sep.join(shard(input))
+
+        auto_insert(self._repo, self._builder, sharded, self._repo.create_blob(content), git.GIT_FILEMODE_BLOB)
+        self._tree = self._builder.write()
+        self.write_commit(input)
 
     def write_commit(self, message: typing.Optional[str] = ""):
         now = int(time.time())
