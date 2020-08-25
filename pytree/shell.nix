ba111dcda5d0bc4eceb935a3e95bd7caafe72533
adisbladis <adisbladis@gmail.com>
2020-08-25 10:20:58 +0200

    Use unstable libgit2
    Needs support for version 1 git repos

--- pytree/shell.nix
+++ pytree/shell.nix
@@ -1,6 +1,22 @@
 { pkgs ? import <nixpkgs> {
   overlays = [
     (import ../../../nix-community/poetry2nix/overlay.nix)
+
+    (self: super: {
+      libgit2 = super.libgit2.overrideAttrs(old: let
+        version = "unstable-2020-09-05";
+      in {
+        name = "libgit2-${version}";
+        inherit version;
+        src = super.fetchFromGitHub {
+          owner = "libgit2";
+          repo = "libgit2";
+          rev = "04d59466238e69c57d2a82d0693a77ecb05e1194";
+          sha256 = "1ppbja0cmw6x8y92zsj4vyr0mwhiq1sinq2vkffryrzjmw4d6r7j";
+        };
+      });      # Bump libgit2 for partial clone support
+    })
+
   ];
 } }:
 
