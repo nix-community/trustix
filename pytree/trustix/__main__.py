1b9cf5dd57422b890e180630ad957df1bd418f49
adisbladis <adisbladis@gmail.com>
2020-08-25 10:58:12 +0200

    Minor test hacks

--- pytree/trustix/__main__.py
+++ pytree/trustix/__main__.py
@@ -1,4 +1,5 @@
 from . import repo
+import os
 
 
 if __name__ == '__main__':
@@ -14,6 +15,9 @@ if __name__ == '__main__':
     def rand_s(n):
         return ''.join(choice(ascii_lowercase) for i in range(n))
 
-    for i in range(1):
+    # for i in range(100 * 1000):
+    for i in range(10 * 1000):
         print(i)
-        r.add_leaf(rand_s(32), rand_s(64).encode())
+        input_hash = rand_s(32)  # Store hash
+        output_hash = os.urandom(32)  # Output NAR hash
+        r.add_leaf(input_hash, output_hash)
