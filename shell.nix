c2d4a5bc801d90724c7b96e2f1953042397b6c63
adisbladis <adisbladis@gmail.com>
2020-09-01 09:31:12 +0200

    Add libgit2 to shell, remove trillian remnants

--- shell.nix
+++ shell.nix
@@ -9,25 +9,16 @@ in pkgs.mkShell {
     pkgs.hivemind  # Process monitoring in development
     pkgs.reflex  # File watcher utility
     pkgs.go
-    pkgs.vgo2nix
+
+    pkgs.libgit2
+    pkgs.pkgconfig
 
     pkgs.protobuf
 
     # For development scripts
     pythonEnv
 
-    # Data store
-    pkgs.mariadb
-    pkgs.trillian
   ];
-
-  TRILLIAN_SCHEMA = "${pkgs.trillian.src}/storage/mysql/schema/storage.sql";
-  MYSQL_BASEDIR = pkgs.mysql;
-
-  MYSQL_ROOT_PASSWORD = "trustix-test-log";
-  MYSQL_DATABASE = "test";
-  MYSQL_USER = "test";
-  MYSQL_PASSWORD = "trustix-test-log";
 
   shellHook = ''
     unset GOPATH
