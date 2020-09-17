92dc0db8287347881d34c7c9ab97af2f2ae8720e
adisbladis <adisbladis@gmail.com>
2020-09-17 11:28:38 +0200

    Fix proto build without manual patching

--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.15
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/dgraph-io/badger/v2 v2.2007.2
+	github.com/golang/protobuf v1.4.2
 	github.com/lazyledger/smt v0.0.0-20200827143353-42131aab296f
 	github.com/libgit2/git2go/v30 v30.0.13
 	github.com/spf13/cobra v1.0.0
+	google.golang.org/grpc v1.33.0-dev
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.0 // indirect
+	google.golang.org/protobuf v1.25.0
 )
--- proto/trustix_grpc.pb.go
+++ proto/trustix_grpc.pb.go
@@ -11,13 +11,16 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-// const _ = grpc.SupportPackageIsVersion7
+const _ = grpc.SupportPackageIsVersion7
 
 // TrustixClient is the client API for Trustix service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TrustixClient interface {
+	// SubmitMapping - Submit an input/output mapping to the local log
 	SubmitMapping(ctx context.Context, in *SubmitRequest, opts ...grpc.CallOption) (*SubmitReply, error)
+	// SubmitMapping - Query a mapping from the local log
+	QueryMapping(ctx context.Context, in *QueryRequest, opts ...grpc.CallOption) (*QueryResponse, error)
 }
 
 type trustixClient struct {
@@ -37,11 +40,23 @@ func (c *trustixClient) SubmitMapping(ctx context.Context, in *SubmitRequest, op
 	return out, nil
 }
 
+func (c *trustixClient) QueryMapping(ctx context.Context, in *QueryRequest, opts ...grpc.CallOption) (*QueryResponse, error) {
+	out := new(QueryResponse)
+	err := c.cc.Invoke(ctx, "/trustix.Trustix/QueryMapping", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // TrustixServer is the server API for Trustix service.
 // All implementations must embed UnimplementedTrustixServer
 // for forward compatibility
 type TrustixServer interface {
+	// SubmitMapping - Submit an input/output mapping to the local log
 	SubmitMapping(context.Context, *SubmitRequest) (*SubmitReply, error)
+	// SubmitMapping - Query a mapping from the local log
+	QueryMapping(context.Context, *QueryRequest) (*QueryResponse, error)
 	mustEmbedUnimplementedTrustixServer()
 }
 
@@ -51,6 +66,9 @@ type UnimplementedTrustixServer struct {
 
 func (UnimplementedTrustixServer) SubmitMapping(context.Context, *SubmitRequest) (*SubmitReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SubmitMapping not implemented")
+}
+func (UnimplementedTrustixServer) QueryMapping(context.Context, *QueryRequest) (*QueryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method QueryMapping not implemented")
 }
 func (UnimplementedTrustixServer) mustEmbedUnimplementedTrustixServer() {}
 
@@ -83,6 +101,24 @@ func _Trustix_SubmitMapping_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Trustix_QueryMapping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TrustixServer).QueryMapping(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/trustix.Trustix/QueryMapping",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TrustixServer).QueryMapping(ctx, req.(*QueryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Trustix_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "trustix.Trustix",
 	HandlerType: (*TrustixServer)(nil),
@@ -90,6 +126,10 @@ var _Trustix_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SubmitMapping",
 			Handler:    _Trustix_SubmitMapping_Handler,
+		},
+		{
+			MethodName: "QueryMapping",
+			Handler:    _Trustix_QueryMapping_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
