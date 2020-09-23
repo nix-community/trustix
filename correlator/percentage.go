d13cfb902e7f702de0d010eedd7f1ae5475e83b8
adisbladis <adisbladis@gmail.com>
2020-09-23 10:12:44 +0200

    Optimise minimum percentage early cutoff

--- correlator/percentage.go
+++ correlator/percentage.go
@@ -33,18 +33,6 @@ func (q *minimumPercent) Decide(inputs []*LogCorrelatorInput) (*LogCorrelatorOut
 		pct int
 	}
 
-	// Filter out any keys with less than minimum percentage match and put in list
-	var matchesMinimum []*sortStruct
-	for k, v := range entries {
-		pct := len(v) * pctPerEntry
-		if pct >= q.minimumPct {
-			matchesMinimum = append(matchesMinimum, &sortStruct{
-				key: k,
-				pct: pct,
-			})
-		}
-	}
-
 	makeReturn := func(m *sortStruct) (*LogCorrelatorOutput, error) {
 		ret := &LogCorrelatorOutput{
 			OutputHash: m.key,
@@ -55,19 +43,33 @@ func (q *minimumPercent) Decide(inputs []*LogCorrelatorInput) (*LogCorrelatorOut
 		return ret, nil
 	}
 
-	if len(matchesMinimum) == 0 {
-		return nil, fmt.Errorf("Could not reach the minimum %d quorum", q.minimumPct)
+	// Filter out any keys with less than minimum percentage match and put in list
+	var matchesMinimum []*sortStruct
+	for k, v := range entries {
+		pct := len(v) * pctPerEntry
+		if pct >= q.minimumPct {
+			m := &sortStruct{
+				key: k,
+				pct: pct,
+			}
+			matchesMinimum = append(matchesMinimum, m)
+			if pct >= 50 || len(inputs) == 1 || len(entries) == 1 || len(inputs) == len(v) {
+				return makeReturn(m)
+			}
+		}
 	}
 
-	// If > 50% only one match is possible, skip sorting
-	if q.minimumPct >= 50 {
+	switch len(matchesMinimum) {
+	case 0:
+		return nil, fmt.Errorf("Could not reach the minimum %d quorum", q.minimumPct)
+	case 1:
+		return makeReturn(matchesMinimum[0])
+	default:
+		// Sort list by the highest match percentage
+		sort.SliceStable(matchesMinimum, func(i, j int) bool {
+			return matchesMinimum[i].pct > matchesMinimum[j].pct
+		})
+
 		return makeReturn(matchesMinimum[0])
 	}
-
-	// Sort list by the highest match percentage
-	sort.SliceStable(matchesMinimum, func(i, j int) bool {
-		return matchesMinimum[i].pct > matchesMinimum[j].pct
-	})
-
-	return makeReturn(matchesMinimum[0])
 }
