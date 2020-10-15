81821cf50879536896ac5c5ad1f0c42bf01a01da
adisbladis <adisbladis@gmail.com>
2020-10-15 14:20:17 +0100

    Add default.nix to build application

---
+++ default.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (import ./nix/overlay.nix)
+    (import "${(builtins.fetchGit {
+      url = "git@github.com:tweag/gomod2nix.git";
+      rev = "1d342d55cd9476af8e165b60a55dbec0d8c10977";
+    })}/overlay.nix")
+  ];
+} }:
+
+let
+  inherit (pkgs) lib;
+
+in pkgs.buildGoApplication {
+  pname = "trustix";
+  version = "dev";
+  src = lib.cleanSource ./.;
+  modules = ./gomod2nix.toml;
+
+  buildInputs = [
+    pkgs.libgit2
+  ];
+
+  nativeBuildInputs = [
+    pkgs.pkgconfig
+  ];
+
+  CGO_ENABLED = "1";
+
+}
