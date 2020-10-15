e33d7bb65fa988bc47829506a3a0bd069c16b1e1
adisbladis <adisbladis@gmail.com>
2020-10-15 15:49:35 +0100

    Don't include tests directory in when building

--- default.nix
+++ default.nix
@@ -14,7 +14,12 @@ let
 in pkgs.buildGoApplication {
   pname = "trustix";
   version = "dev";
-  src = lib.cleanSource ./.;
+
+  src = lib.cleanSourceWith {
+    filter = name: type: ! lib.hasSuffix "tests" name;
+    src = lib.cleanSource ./.;
+  };
+
   modules = ./gomod2nix.toml;
 
   buildInputs = [
