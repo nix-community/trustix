08ef33df7001a555f43d969ac3a74f600d7fbc75
adisbladis <adisbladis@gmail.com>
2020-12-18 17:09:28 +0100

    nix: Switch to HTTPS Github urls
    We don't want users to have to auth.

--- nix/default.nix
+++ nix/default.nix
@@ -12,11 +12,11 @@ let
     overlays = [
       (import ./overlay.nix)
       (import "${(builtins.fetchGit {
-        url = "git@github.com:tweag/gomod2nix.git";
+        url = "https://github.com/tweag/gomod2nix.git";
         rev = "929d740884811b388acc0f037efba7b5bc5745e8";
       })}/overlay.nix")
       (import "${(builtins.fetchGit {
-        url = "git@github.com:nix-community/poetry2nix.git";
+        url = "https://github.com/nix-community/poetry2nix.git";
         rev = "31e3d16d65e65a11ae8e1aaeb9c2e7748144617f";
       })}/overlay.nix")
     ];
