83f07f746b4a6fb741c7682b91dd72ed726714bf
adisbladis <adisbladis@gmail.com>
2021-03-15 13:20:32 +0200

    contrib/ui-dashboard: Give noscript users a better experience by omiting the css bundle loading

--- contrib/ui-dashboard/trustix_dash/js/src/main.ts
+++ contrib/ui-dashboard/trustix_dash/js/src/main.ts
@@ -4,6 +4,16 @@ import { initSuggestions } from "./suggestions"
 import { initTooltips } from "./tooltips"
 
 function init() {
+  // While it may look like we should load this CSS from base.jinja2
+  // it's entirely dependent on the javascript loading.
+  // Give noscript users a better experience by completely omiting the css
+  // loading
+  const link = document.createElement("link")
+  link.href = "/js/bundle.css"
+  link.type = "text/css"
+  link.rel = "stylesheet"
+  document.getElementsByTagName("head")[0].appendChild(link)
+
   renderAttrCharts()
   renderDrvCharts()
   initSuggestions()
