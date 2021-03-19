28ce7cdd25fbdfb40286a45c7cbedf11cc3b2aff
adisbladis <adisbladis@gmail.com>
2021-03-19 14:28:48 +0200

    packages/trustix-proto: Update year in copyright headers

--- packages/trustix-proto/tools.go
+++ packages/trustix-proto/tools.go
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2020 Tweag IO
+// Copyright (c) 2021 Tweag IO
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
