e1fdaeedf83d0f8fadb0db3ae7dcf295da6e7fbf
adisbladis <adisbladis@gmail.com>
2021-05-25 22:04:18 -0500

    Expose container attributes in root level Nix expression

--- default.nix
+++ default.nix
@@ -1,5 +1,22 @@
 { pkgs ? import ./nix }:
 
 {
-  inherit (pkgs) trustix trustix-doc trustix-nix trustix-nix-reprod;
+  packages = {
+    inherit (pkgs) trustix trustix-doc trustix-nix trustix-nix-reprod;
+  };
+
+  containers =
+    let
+      inheritContainer = attr: {}@args: pkgs.dockerTools.buildLayeredImage
+        {
+          inherit (pkgs.${attr}) name;
+          contents = [ pkgs.${attr} ];
+        } // args;
+    in
+    {
+      trustix = inheritContainer "trustix" { };
+      trustix-nix = inheritContainer "trustix-nix" { };
+      trustix-nix-reprod = inheritContainer "trustix-nix-reprod" { };
+    };
+
 }
