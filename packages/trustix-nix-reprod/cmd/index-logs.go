2f02686db93695d10e126dcbdb3737e3efeae987
adisbladis <adisbladis@gmail.com>
2022-09-01 14:17:40 +1200

    trustix-nix-reprod: Update copyright headers
    Tweag are no longer hiring me and do not own the copyright to new developments.

--- packages/trustix-nix-reprod/cmd/index-logs.go
+++ packages/trustix-nix-reprod/cmd/index-logs.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Tweag IO
+// Copyright (C) 2022 adisbladis
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
 //
