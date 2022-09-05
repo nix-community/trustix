e2330bfafa9fde5a7d0d28dca8b2a6057787a2b5
adisbladis <adisbladis@gmail.com>
2022-09-06 03:43:36 +1200

    trustix-nix-reprod: Fully kill off the old python implementation
    There is no point keeping it around in the tree even if it's not fully replaced yet.

--- packages/trustix-nix-reprod/old-python-impl/trustix_nix_reprod/js/src/iframe-resize.ts
+++
@@ -1,5 +0,0 @@
-export function resizeIFrames(): void {
-  document.querySelectorAll("iframe.seamless").forEach((elem: HTMLIFrameElement) => {
-    elem.style.height = elem.contentWindow.document.body.scrollHeight + 50 + 'px';
-  })
-}
