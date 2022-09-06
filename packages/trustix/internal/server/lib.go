bb4b49ae92ad4dc803d75987f95f40ee24806c96
adisbladis <adisbladis@gmail.com>
2022-09-06 17:40:59 +1200

    treewide: Create license headers automatically with tools/fix_licenses

--- packages/trustix/internal/server/lib.go
+++ packages/trustix/internal/server/lib.go
@@ -1,10 +1,7 @@
 // Copyright (C) 2021 Tweag IO
+// Copyright © 2020-2022 The Trustix Authors
 //
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-3.0-only
 
 package server
 
