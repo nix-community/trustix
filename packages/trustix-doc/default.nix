a0a2b496546c6af105b0c1d1bcb51a191d8a6af5
adisbladis <adisbladis@gmail.com>
2022-09-06 18:06:07 +1200

    trustix-doc: Fix build

--- packages/trustix-doc/default.nix
+++ packages/trustix-doc/default.nix
@@ -1,21 +1,24 @@
-{ pkgs ? import ../../nix, lib ? pkgs.lib }:
+{ pkgs ? import ../../pkgs.nix { }
+, lib ? pkgs.lib
+}:
 
-let
-  rootShell = import ../../shell.nix;
-
-in
 pkgs.stdenv.mkDerivation {
   pname = "trustix-doc";
   version = "dev";
 
-  src = ./.;
+  src = lib.cleanSource ./.;
 
   nativeBuildInputs = [
     pkgs.mdbook
   ];
 
-  preBuild = ''
+  buildPhase = ''
+    runHook preBuild
+
     ln -s ${lib.cleanSource ../trustix-proto} ../trustix-proto
+    mdbook build
+
+    runHook postBuild
   '';
 
   installPhase = ''
