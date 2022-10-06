3ca33f26a47746c3801a2ebf77f9cf43fb55e3bf
adisbladis <adisbladis@gmail.com>
2022-10-06 22:22:45 +1300

    Add gitignore to flake inputs

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix, npmlock2nix, nix-eval-jobs }@flakeInputs:
+  outputs = { self, nixpkgs, flake-utils, gomod2nix, npmlock2nix, gitignore, nix-eval-jobs }@flakeInputs:
     (flake-utils.lib.eachDefaultSystem
       (system:
         let
