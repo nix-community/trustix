4438451e8f623f5471528e253912d5157d048e8b
adisbladis <adisbladis@gmail.com>
2022-10-19 00:39:44 +1300

    infra: use trustix-nix-r13y nixos module for demo instance

--- infra/trustix.dev/trustix.dev.nix
+++ infra/trustix.dev/trustix.dev.nix
@@ -1,7 +1,28 @@
 { config, pkgs, ... }:
 
 {
+  # Static website for trustix.dev
   services.nginx.virtualHosts."trustix.dev" = {
     root = pkgs.trustix-doc;
+  };
+
+  # Reproducibility dashboard
+  services.trustix-nix-r13y = {
+    enable = true;
+    hostName = "r13y.trustix.dev";
+
+    lognames = {
+      e0f263745e4e3ab07ab5275b00b44f594e0b6d2bd35892a8ebd10a7f86322eb7 = "trustix-demo";
+    };
+
+    attrs = {
+      nixos-unstable = [ "nixos.iso_minimal.x86_64-linux" ];
+    };
+
+    channels.hydra.nixos-unstable = {
+      base_url = "https://hydra.nixos.org";
+      jobset = "trunk-combined";
+      project = "nixos";
+    };
   };
 }
