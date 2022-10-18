09046a9d7c74c195fc3b644327d248ecf497cd0a
adisbladis <adisbladis@gmail.com>
2022-10-18 23:14:28 +1300

    trustix-nix-r13y: add all runtime $PATH deps to wrapper

--- packages/trustix-nix-r13y/default.nix
+++ packages/trustix-nix-r13y/default.nix
@@ -1,12 +1,35 @@
-{ buildGoApplication, lib, gitignoreSource }:
+{ buildGoApplication
+, lib
+, gitignoreSource
+, makeWrapper
+, nix-eval-jobs
+, nix
+, diffoscope
+}:
 
+let
+  runtimeDeps = [
+    nix
+    nix-eval-jobs
+    diffoscope
+  ];
+
+in
 buildGoApplication {
   pname = "trustix-nix-r13y";
   version = "dev";
   pwd = ./.;
+  modules = ./gomod2nix.toml;
   src = lib.cleanSourceWith {
     filter = name: type: ! lib.hasSuffix "tests" name;
     src = gitignoreSource ./.;
   };
-  modules = ./gomod2nix.toml;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  postInstall = ''
+    wrapProgram "$out/bin/trustix-nix-r13y" --prefix PATH : "${lib.makeBinPath runtimeDeps}"
+  '';
 }
