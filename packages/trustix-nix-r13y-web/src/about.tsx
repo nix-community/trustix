461ff73fcb5904e92042626d232d278ef55aa6e1
adisbladis <adisbladis@gmail.com>
2022-10-20 17:09:20 +1300

    trustix-nix-r13y-web: prettier format

--- packages/trustix-nix-r13y-web/src/about.tsx
+++ packages/trustix-nix-r13y-web/src/about.tsx
@@ -16,12 +16,12 @@ const About: Component = () => {
       <span class="py-4 mx-1">
         <h2 class="text-lg font-bold">What is this?</h2>
         <p>
-          This tool aggregates data from multiple <A href={homepageURL}>Trustix</A> log operators (i.e. builders) and
+          This tool aggregates data from multiple{" "}
+          <A href={homepageURL}>Trustix</A> log operators (i.e. builders) and
           cross compares them to establish reproducibility.
         </p>
         <p>
-          Builds are run on a
-          variety of platforms and hardware configurations.
+          Builds are run on a variety of platforms and hardware configurations.
         </p>
       </span>
 
@@ -32,13 +32,13 @@ const About: Component = () => {
         <p>
           Fairly. We don't know exactly how many reproducibility issues are
           being exercised already, but it's fair to say quite a few. <br />
-          The quality of the results depend on the diverseness of builders,
-          If all builders run the same CPU model the quality will be lower
+          The quality of the results depend on the diverseness of builders, If
+          all builders run the same CPU model the quality will be lower
         </p>
         <br />
         <p>
-          It isn't possible to guarantee a package is reproducible, just like it isn't
-          possible to prove software is bug-free. It is possible there is
+          It isn't possible to guarantee a package is reproducible, just like it
+          isn't possible to prove software is bug-free. It is possible there is
           nondeterminism in a package source, waiting for some specific
           circumstance.
         </p>
@@ -48,25 +48,33 @@ const About: Component = () => {
         <h2 class="text-lg font-bold">How can I help?</h2>
         <p>
           You can start submitting builds to your own Trustix log and add the
-          log to the <A class="underline" href={officialInstanceURL}>official Trustix R13y instance</A>.
+          log to the{" "}
+          <A class="underline" href={officialInstanceURL}>
+            official Trustix R13y instance
+          </A>
+          .
           <br />
           It's especially helpful if you are bootstrapping (not using
           cache.nixos.org) for any substitutions, but every bit helps.
         </p>
         <br />
         <p>
-
-          Another thing you can do is to report issues and help to fix them at <A class="underline" href={homepageURL}>{homepageURL}</A>.
+          Another thing you can do is to report issues and help to fix them at{" "}
+          <A class="underline" href={homepageURL}>
+            {homepageURL}
+          </A>
+          .
         </p>
       </span>
 
       <span class="py-4 mx-1">
         <h2 class="text-lg font-bold">What about the name?</h2>
         <p>
-          It's a homage to <A href="https://r13y.com">r13y.com</A> by <A href="https://github.com/grahamc">@grahamc</A> which started the reproducibility tracking effort for Nix/NixOS.
+          It's a homage to <A href="https://r13y.com">r13y.com</A> by{" "}
+          <A href="https://github.com/grahamc">@grahamc</A> which started the
+          reproducibility tracking effort for Nix/NixOS.
         </p>
       </span>
-
     </>
   );
 };
